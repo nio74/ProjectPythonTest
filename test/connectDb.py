216c765547281ef179f5322640ca7fecaeae94cd
Paolo <nio74maz@hotmail.it>
2020-12-11 13:32:37 +0100

    casa

--- test/connectDb.py
+++ test/connectDb.py
@@ -1,4 +1,4 @@
 import pyodbc
 
-conn = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\Ron\Desktop\Test\testdb.accdb;')
+conn = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\nio74\Desktop\datiprova.mdb;PWD=gmpa;')
 cursor = conn.cursor()
